f9a900a7f0963c583e7e8072f535c477b0b7637f
Pavithra2294 <pav.ganesan@gmail.com>
2025-11-07 12:53:55 +0000

    Add files via upload

---
+++ terraform/github.tf
@@ -0,0 +1,11 @@
+provider "github" {
+  owner = "Pavithra2294"
+}
+
+
+resource "github_repository" "demo_repo11" {
+  name        = "my-terraform-created-repo-demo1111"
+  description = "This repository was created using Terraform!"
+  visibility  = "public"    # or "private"
+  auto_init   = true        # creates an initial commit with README
+}
---
+++ terraform/main.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "pavithra-terraform-demo-bucket2"
+}
